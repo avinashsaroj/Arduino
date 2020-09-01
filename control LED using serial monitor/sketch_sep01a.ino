a121ceacc67d7d8171fa512128b576d1d49683f0
Avinash <sarojavinash1995@gmail.com>
2020-09-01 11:55:33 +0530

    new assignment

---
+++ control LED using serial monitor/sketch_sep01a.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  if (Serial.available() > 0) {
+    char state = Serial.read();
+    if (state == '1') {
+      digitalWrite(13, HIGH);
+      Serial.println("LED ON");
+    }
+    if (state == '0') {
+      digitalWrite(13, LOW);
+      Serial.println("LED OFF");
+    }
+  }
+  
+}
